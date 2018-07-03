429c324cbcd9467aa7402cfe4ed5e731609335d0
Erik Karlkvist <erka@netlight.com>
2018-07-03 13:05:57 +0200

    created auth functions

--- app/src/backend/users.js
+++ app/src/backend/users.js
@@ -1,7 +1,5 @@
 import firebase from "./firebase"
 
-let isLoggedIn = false;
-
 export async function getUser(uid){
   const snapshot = await firebase.firestore().collection("users").doc(uid).get();
   if(snapshot.exists){
@@ -14,31 +12,3 @@ export async function getUser(uid){
     console.log("No such document!");
   }
 }
-
-export async function login(email, password){
-  await firebase.auth().signInWithEmailAndPassword(email, password).catch(function(error) {
-    return Promise.reject(error);
-  });
-  console.log(firebase.auth())
-  return Promise.resolve(true)
-}
-
-export async function logout(){
-  await firebase.auth().signOut().catch(function(error) {
-    return Promise.reject(error);
-  });
-  console.log(firebase.auth())
-  return Promise.resolve(true)
-}
-
-export function isLoggedIn(){
-  return isLoggedIn;
-}
-
-firebase.auth().onAuthStateChanged(function(user) {
-if (user) {
-    isLoggedIn = true;
-  } else {
-    isLoggedIn = false;
-  }
-});
