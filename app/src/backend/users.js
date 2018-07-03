137eff6c8931cbe930a8fe752e30a91d6eb4a562
Jostein Brevik <jbre@netlight.com>
2018-07-03 14:04:42 +0200

    Add getAllUsers

--- app/src/backend/users.js
+++ app/src/backend/users.js
@@ -1,10 +1,14 @@
-import firebase from "./firebase"
+import firebase from "./firebase";
 
 let isLoggedIn = false;
 
-export async function getUser(uid){
-  const snapshot = await firebase.firestore().collection("users").doc(uid).get();
-  if(snapshot.exists){
+export async function getUser(uid) {
+  const snapshot = await firebase
+    .firestore()
+    .collection("users")
+    .doc(uid)
+    .get();
+  if (snapshot.exists) {
     const userData = snapshot.data();
     userData.id = snapshot.id;
     console.log("Document data:", userData);
@@ -15,28 +19,52 @@ export async function getUser(uid){
   }
 }
 
-export async function login(email, password){
-  await firebase.auth().signInWithEmailAndPassword(email, password).catch(function(error) {
-    return Promise.reject(error);
+export async function getAllUsers() {
+  const snapshots = await firebase
+    .firestore()
+    .collection("users")
+    .get();
+  const users = [];
+  snapshots.forEach(snapshot => {
+    if (snapshot.exists) {
+      const data = snapshot.data();
+      data.id = snapshot.id;
+      users.push(data);
+    }
   });
-  console.log(firebase.auth())
-  return Promise.resolve(true)
+
+  console.log(users);
+  return Promise.resolve(users);
 }
 
-export async function logout(){
-  await firebase.auth().signOut().catch(function(error) {
-    return Promise.reject(error);
-  });
-  console.log(firebase.auth())
-  return Promise.resolve(true)
+export async function login(email, password) {
+  await firebase
+    .auth()
+    .signInWithEmailAndPassword(email, password)
+    .catch(function(error) {
+      return Promise.reject(error);
+    });
+  console.log(firebase.auth());
+  return Promise.resolve(true);
 }
 
-export function isLoggedIn(){
+export async function logout() {
+  await firebase
+    .auth()
+    .signOut()
+    .catch(function(error) {
+      return Promise.reject(error);
+    });
+  console.log(firebase.auth());
+  return Promise.resolve(true);
+}
+
+export function isLoggedIn() {
   return isLoggedIn;
 }
 
 firebase.auth().onAuthStateChanged(function(user) {
-if (user) {
+  if (user) {
     isLoggedIn = true;
   } else {
     isLoggedIn = false;
