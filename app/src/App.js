1743c464512d8b2b03b8f119c4828242729864ec
Jostein Brevik <jbre@netlight.com>
2018-07-03 14:04:13 +0200

    Add get and display all projects and users

--- app/src/App.js
+++ app/src/App.js
@@ -1,29 +1,48 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
-import {getUser} from "./backend/users"
-import {getAllProjects, getProject} from "./backend/projects"
-import {login, logout} from "./backend/users";
-import firebase from "./backend/firebase"
+import React, { Component } from "react";
+import logo from "./logo.svg";
+import "./App.css";
+import { getUser } from "./backend/users";
+import { getAllProjects, getProject } from "./backend/projects";
+import { login, logout, getAllUsers } from "./backend/users";
+import firebase from "./backend/firebase";
+import ProjectsDisplay from "./components/ProjectsDisplay";
 
-login("henninnenesgrd@gmail.com", "123456789")
+login("henninnenesgrd@gmail.com", "123456789");
 //https://api.trello.com/1/members/me/?key=85873074232e857f4e364a3ef1b545a3&token=ff6a08c9b2fc1df53138d4f788122032fd7d7ae93bb6919f1d81d1c5cd8af10b
 class App extends Component {
-
-  constructor(){
+  constructor() {
     super();
     firebase.auth().onAuthStateChanged(function(user) {
-    if (user) {
+      if (user) {
         // User is signed in.
-        console.log("USER IS SIGNED IN")
+        console.log("USER IS SIGNED IN");
         // ...
       } else {
-        console.log("USER IS SIGNED OUT")
+        console.log("USER IS SIGNED OUT");
       }
+    });
+    this.state = { projects: [], users: [] };
+  }
+
+  componentDidMount() {
+    getAllProjects().then(projects => {
+      this.setState({ projects });
+    });
+
+    getAllUsers().then(users => {
+      console.log("USERS");
+
+      //Gets name so that this works with ProjectsDisplay. Should probably use setState({users})
+      const usernames = users.map(d => {
+        return { name: d.firstname + " " + d.lastname };
+      });
+      this.setState({ users: usernames });
     });
   }
 
   render() {
+    console.log("!", this.state);
+
     return (
       <div className="App">
         <header className="App-header">
@@ -33,6 +52,8 @@ class App extends Component {
         <p className="App-intro">
           To get started, edit <code>src/App.js</code> and save to reload.
         </p>
+        <ProjectsDisplay projects={this.state.projects} />
+        <ProjectsDisplay projects={this.state.users} />
       </div>
     );
   }
