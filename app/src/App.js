bbf3da769facd703e64bc305b77524978767f92f
Henning Nesgård <henningnesgard@Hennings-MacBook-Pro.local>
2018-07-04 09:56:36 +0200

    added fonts.css

--- app/src/App.js
+++ app/src/App.js
@@ -5,8 +5,9 @@ import {getUser} from "./backend/users"
 import {getAllProjects, getProject} from "./backend/projects"
 import firebase from "./backend/firebase"
 import {login, logut, register} from "./backend/auth"
+import "./resources/fonts.css"
 
-//register("Henning", "Nesgård", "henninnenesgrd@gmail.com", "tester");
+//register("Henning", "Nesgård", "lol@gmail.com", "tester");
 //login("henninnenesgrd@gmail.com", "123456789")
 //https://api.trello.com/1/members/me/?key=85873074232e857f4e364a3ef1b545a3&token=ff6a08c9b2fc1df53138d4f788122032fd7d7ae93bb6919f1d81d1c5cd8af10b
 class App extends Component {
@@ -29,8 +30,9 @@ class App extends Component {
       <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
+          
         </header>
+        <h1>Welcome to React</h1>
         <p className="App-intro">
           To get started, edit <code>src/App.js</code> and save to reload.
         </p>
