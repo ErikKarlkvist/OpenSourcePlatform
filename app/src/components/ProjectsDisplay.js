6b66462fa9a06897f8491c208c69861d89aadbd5
Erik Karlkvist <erka@netlight.com>
2018-07-04 10:57:40 +0200

    bootstrap init

--- app/src/components/ProjectsDisplay.js
+++ app/src/components/ProjectsDisplay.js
@@ -3,9 +3,20 @@ import React, { Component } from "react";
 class ProjectsDisplay extends Component {
   render() {
     const data = this.props.projects;
-    console.log(data);
-    const listItems = data.map(d => <li key={d.name}>{d.name}</li>);
-    return <div>{listItems}</div>;
+
+    const items = data.map(d =>
+      <div class="col-4">
+        {d.name}
+      </div>
+    );
+
+    return (
+      <div class="container">
+        <div class="row">
+          {items}
+        </div>
+      </div>
+    );
   }
 }
 export default ProjectsDisplay;
