cba5ec209b1d81d87008bffea2139509c07c07cb
Erik Karlkvist <erka@osl-m-erka.local>
2018-07-04 10:39:17 +0200

    header homepage

--- app/src/pages/HomePage.js
+++ app/src/pages/HomePage.js
@@ -1,16 +1,9 @@
 import React, { Component } from "react";
 import logo from "../logo.svg";
 import "./HomePage.css";
-import { getUser } from "../backend/users";
 import { getAllProjects, getProject } from "../backend/projects";
-import { login, logout, getAllUsers } from "../backend/users";
-import firebase from "../backend/firebase";
 import ProjectsDisplay from "../components/ProjectsDisplay";
-
-//getAllProjects()
-//register("Henning", "Nesgård", "henninnenesgrd@gmail.com", "tester");
-//login("henninnenesgrd@gmail.com", "123456789")
-//https://api.trello.com/1/members/me/?key=85873074232e857f4e364a3ef1b545a3&token=ff6a08c9b2fc1df53138d4f788122032fd7d7ae93bb6919f1d81d1c5cd8af10b
+import LoginRegister from "../components/LoginRegister"
 class HomePage extends Component {
   constructor() {
     super();
@@ -27,15 +20,18 @@ class HomePage extends Component {
 
   render() {
     return (
-      <div class="App">
+      <div class="HomePage">
         <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
+          <img src={logo} className="Logo" alt="logo" />
+          <LoginRegister />
         </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-        <ProjectsDisplay projects={this.state.projects} />
+        <div class="Content">
+          <h1 className="App-title">DNB Open Source</h1>
+          <h2 className="App-intro">
+            Give your contribution
+          </h2>
+          <ProjectsDisplay projects={this.state.projects} />
+        </div>
       </div>
     );
   }
