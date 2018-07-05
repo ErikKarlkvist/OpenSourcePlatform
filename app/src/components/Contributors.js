c6a930d15b3098f562895229731df09e2c8663e4
Erik Karlkvist <erka@osl-m-erka.local>
2018-07-05 13:01:20 +0200

    started contributors

---
+++ app/src/components/Contributors.js
@@ -0,0 +1,37 @@
+import React from 'react';
+import PropTypes from 'prop-types'
+
+class Spinner extends React.Component {
+
+  static propTypes = {
+    contributors: PropTypes.object.isRequired
+  }
+
+  render() {
+
+    return (
+      <div>
+        <h3> Contributors</h3>
+      </div>
+    )
+
+  }
+}
+
+const styles = {
+  fillPage: {
+    position: "absolute",
+    top: 0,
+    left: 0,
+    width: "100%",
+    height: "100%",
+    justifyContent: "center",
+    alignItems: "center",
+    display: "flex",
+  },
+  Contributors: {
+    marginTop: "30px"
+  }
+}
+
+export default Spinner;
