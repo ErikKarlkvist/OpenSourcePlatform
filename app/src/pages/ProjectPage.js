243e5cfb5d61d66ba152e91487fa550f53860180
Jostein Brevik <jbre@netlight.com>
2018-07-05 09:37:22 +0200

    ProjectBlurb styling (shadows)

--- app/src/pages/ProjectPage.js
+++ app/src/pages/ProjectPage.js
@@ -4,42 +4,39 @@ import "./Main.css";
 import { getProject } from "../backend/projects";
 import LoginRegister from "../components/LoginRegister";
 import { BrowserRouter as Router, Route, Link } from "react-router-dom";
-import { withRouter } from 'react-router'
-import PropTypes from 'prop-types'
-import Spinner from "../components/Spinner"
+import { withRouter } from "react-router";
+import PropTypes from "prop-types";
+import Spinner from "../components/Spinner";
 
 class ProjectPage extends Component {
-
   static propTypes = {
     match: PropTypes.object.isRequired,
     location: PropTypes.object.isRequired,
     history: PropTypes.object.isRequired
-  }
+  };
 
   constructor(props) {
     super(props);
-    console.log(props)
+    console.log(props);
     this.state = {
       loading: true,
       project: {}
-    }
+    };
   }
 
-  componentDidMount(){
-
-    if(this.props.match && this.props.match.params.projectId){
-      getProject(this.props.match.params.projectId).then((project) => {
+  componentDidMount() {
+    if (this.props.match && this.props.match.params.projectId) {
+      getProject(this.props.match.params.projectId).then(project => {
         this.setState({
           loading: false,
           project
-        })
-      })
+        });
+      });
     }
-
   }
 
   render() {
-    console.log(this.state)
+    console.log(this.state);
     return (
       <div class="PageContainer">
         <header className="App-header">
@@ -48,7 +45,7 @@ class ProjectPage extends Component {
         </header>
         <div class="Content">
           <h1>{this.state.project.name}</h1>
-          <Spinner/>
+          {this.state.spinner && <Spinner />}
         </div>
       </div>
     );
