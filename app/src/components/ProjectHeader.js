0dcd2558c1633a435f0e995b59816f7716cf9085
Erik Karlkvist <erka@netlight.com>
2018-07-05 11:17:19 +0200

    header

--- app/src/components/ProjectHeader.js
+++ app/src/components/ProjectHeader.js
@@ -1,6 +1,6 @@
 import React, { Component } from "react";
 import ProjectBlurb from "./ProjectBlurb";
-import "./ProjectsDisplay.css";
+import "../resources/Main.css";
 import LoginRegister from "../components/LoginRegister"
 import logo from "../logo.svg";
 
@@ -10,19 +10,49 @@ class ProjectsDisplay extends Component {
 
   }
 
-
   render() {
 
     return (
-      <div>
-        <img src={logo} className="Logo" alt="logo" />
-        <LoginRegister />
-        <div>
-          <h1>{this.props.name}</h1>
-        </div>
+      <div style={styles.HeaderContainer}>
+
+          <img style={styles.HeaderImage} src={this.props.headerImageURL}/>
+
+          <div style={styles.HeaderContent}>
+            <header class = "App-header">
+              <img src={logo} class="Logo" alt="logo" />
+              <LoginRegister />
+            </header>
+              <h2 style={{marginTop: 20}}>{this.props.name}</h2>
+              <h3 style={{marginTop: 80}}>Creator: "not implemented"</h3>
+          </div>
+
+
       </div>
     );
   }
 }
+
+const styles = {
+  HeaderContainer: {
+    position: "relative",
+    width: "100%",
+    height: "50vh",
+    backgroundColor: "#00343e"
+  },
+  HeaderImage: {
+    position: "relative",
+    width: "100%",
+    height: "100%",
+    opacity: 0.4
+  },
+  HeaderContent: {
+    //Centers the text above the image
+    position: "absolute",
+    width: "100%",
+    left: "0",
+    top: "0",
+    textAlign: "center"
+  },
+};
 
 export default ProjectsDisplay;
