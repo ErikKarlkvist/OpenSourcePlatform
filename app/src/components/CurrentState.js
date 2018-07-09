74a9ad42d25458471104bc760cc5f70b3aa7b6e4
Jostein Brevik <jbre@netlight.com>
2018-07-09 17:02:48 +0200

    Add show more/less to Current State thumbnails

--- app/src/components/CurrentState.js
+++ app/src/components/CurrentState.js
@@ -2,44 +2,71 @@ import React, { Component } from "react";
 import Slider from "react-slick";
 import "./CurrentState.css";
 
+//Sets how many pictures are shown if "show more" has not been pressed
+const cutoff = 1;
+
 class CurrentState extends Component {
+  constructor() {
+    super();
+    this.state = { images: [], showing: [] };
+  }
+
+  componentDidMount() {
+    const data = this.props.project.thumbnails;
+    let items = [];
+    if (data) {
+      items = data.map(d => (
+        <div class="Thumbnail col-md-6 col-sm-12 col-lg-4">
+          <img style={styles.Image} src={d.url} />
+        </div>
+      ));
+    }
+    this.setState({ images: items, showing: items.slice(0, cutoff) });
+  }
+
+  showAll = () => {
+    this.setState({ showing: this.state.images });
+  };
+
+  showLess = () => {
+    this.setState({ showing: this.state.images.slice(0, cutoff) });
+  };
+
   render() {
-    const settings = {
-      dots: true,
-      infinite: true,
-      speed: 500,
-      slidesToShow: 3,
-      slidesToScroll: 3
-    };
+    if (!this.state.images) {
+      return <div />;
+    }
+
     return (
       <div className="currentStateContainer">
-        <h2
-          style={{
-            paddingBottom: "30px",
-            textAlign: "left"
-          }}
-        >
-          Current State
-        </h2>
+        <h2 style={styles.Header}>Current State</h2>
+        <div class="row">{this.state.showing}</div>
 
-        <Slider settings={settings}>
-          <div>1</div>
-          <div>2</div>
-          <div>3</div>
-          <div>
-            <h3>4</h3>
-          </div>
-          <div>
-            <h3>5</h3>
-          </div>
-          <div>
-            <h3>6</h3>
-          </div>
-        </Slider>
+        {this.state.images.length > cutoff &&
+          this.state.showing.length < this.state.images.length && (
+            <a onClick={this.showAll}>Show more</a>
+          )}
+        {this.state.showing.length > cutoff && (
+          <a onClick={this.showLess} className="ShowLess">
+            Show less
+          </a>
+        )}
       </div>
     );
   }
 }
-const styles = {};
+const styles = {
+  Image: {
+    border: "1px solid white",
+    width: "100%",
+    height: "100%"
+  },
+  Name: {
+    color: "grey"
+  },
+  Header: {
+    textAlign: "left"
+  }
+};
 
 export default CurrentState;
