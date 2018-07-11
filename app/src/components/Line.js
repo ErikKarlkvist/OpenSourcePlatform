6532a618d5a0714378da1a3a24e409644dadd01f
Erik Karlkvist <erka@osl-m-erka.local>
2018-07-11 15:06:30 +0200

    line

--- app/src/components/Line.js
+++ app/src/components/Line.js
@@ -2,21 +2,32 @@ import React, { Component } from "react";
 
 class Line extends Component {
   render() {
+    let style = styles.small;
+    if (this.props.full) {
+      style = styles.full;
+    }
     //hooked with login
     return (
       <div style={this.props.style}>
-        <div style={styles.line} />
+        <div style={style} />
       </div>
     );
   }
 }
 
 const styles = {
-  line: {
+  small: {
     borderBottom: "1px solid white",
     width: "50%",
     margin: "auto",
     display: "flex"
+  },
+  full: {
+    width: "100%",
+    borderBottom: "2px solid white",
+    position: "absolute",
+    left: 0,
+    right: 0
   }
 };
 export default Line;
