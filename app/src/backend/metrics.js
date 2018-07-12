bef32b27c117fe5d674415c48b732bf9cb4b8e12
Erik Karlkvist <erka@osl-m-erka.local>
2018-07-12 16:24:26 +0200

    project metrics

--- app/src/backend/metrics.js
+++ app/src/backend/metrics.js
@@ -2,18 +2,37 @@
 see https://developer.github.com/v3/issues/#list-issues
 url = URL TO GITHUB
 page >= 1 || undefined
+type = "bug", "enhancement", "question" etc
 WARNING: ONLY WORKS WHEN LABEL "ENHANCEMENT" IS SET ON GITHUB ISSUES
 */
-async function getFeatureRequestsFromGithub(url, page) {
+async function getIssueFromGithub(url, page, type) {
   //input example url
   //https://github.com/adam-golab/react-developer-roadmap
   const gitRepo = url.replace("https://github.com", "");
   if (!page) {
     page = 1;
   }
-  const urlToOpenFeatureRequests = `https://api.github.com/repos${gitRepo}/issues?state=open&labels=bug&page=${page}`;
-  let result = await fetch(urlToOpenFeatureRequests);
-  result = result.json();
+
+  if (!type) {
+    throw new Error("No type specified");
+  }
+
+  const urlToOpenIssues = `https://api.github.com/repos${gitRepo}/issues?state=open&labels=${type}&page=${page}`;
+  let result = await fetch(urlToOpenIssues);
+  result = await result.json();
+  return Promise.resolve(result);
+}
+
+// see https://developer.github.com/v3/repos/collaborators/#list-collaborators
+// url = github url
+async function getCollaborators(url) {
+  const gitRepo = url.replace("https://github.com", "");
+  const urlToCollabs = `https://api.github.com/repos${gitRepo}/contributors`;
+  console.log(urlToCollabs);
+  let result = await fetch(urlToCollabs);
+  console.log(result);
+  result = await result.json();
+  console.log(result);
   return Promise.resolve(result);
 }
 
@@ -23,15 +42,37 @@ export async function getFeatureRequestCount(url) {
 
   let features = [];
   let started = false;
-  console.log("hello");
   while (features.length === 30 || !started) {
     console.log("running");
     started = true;
-    features = await getFeatureRequestsFromGithub(url, index);
+    features = await getIssueFromGithub(url, index, "enhancement");
     console.log(features);
     count += features.length;
     index++;
   }
+
+  return Promise.resolve(count);
+}
+
+export async function getBugCount(url) {
+  let count = 0;
+  let index = 1;
+
+  let bugs = [];
+  let started = false;
+  while (bugs.length === 30 || !started) {
+    started = true;
+    bugs = await getIssueFromGithub(url, index, "bug");
+    count += bugs.length;
+    index++;
+  }
+
+  return Promise.resolve(count);
+}
+
+export async function getCollaboratorCount(url) {
+  const collaborators = await getCollaborators(url);
+  const count = collaborators.length;
 
   return Promise.resolve(count);
 }
--- app/src/components/ProjectMetrics.js
+++ app/src/components/ProjectMetrics.js
@@ -1,16 +1,23 @@
 import React, { Component } from "react";
-import { getFeatureRequestCount } from "../backend/metrics";
-
+import {
+  getFeatureRequestCount,
+  getBugCount,
+  getCollaboratorCount
+} from "../backend/metrics";
+import "../resources/fonts.css";
+import "../resources/colors.css";
 class ProjectMetrics extends Component {
   constructor() {
     super();
     this.state = {
-      nmbrOfEts: ""
+      nmbrOfEts: "",
+      nmbrOfBugs: "",
+      nmbrOfClb: ""
     };
   }
 
   componentDidMount() {
-    getFeatureRequestCount("https://github.com/browsh-org/browsh")
+    getFeatureRequestCount(this.props.gitURL)
       .then(res => {
         this.setState({ nmbrOfEts: res });
       })
@@ -18,6 +25,28 @@ class ProjectMetrics extends Component {
         console.log(e);
         this.setState({
           nmbrOfEts: "Couldn't fetch"
+        });
+      });
+
+    getBugCount(this.props.gitURL)
+      .then(res => {
+        this.setState({ nmbrOfBugs: res });
+      })
+      .catch(e => {
+        console.log(e);
+        this.setState({
+          nmbrOfBugs: "Couldn't fetch"
+        });
+      });
+
+    getCollaboratorCount(this.props.gitURL)
+      .then(res => {
+        this.setState({ nmbrOfClb: res });
+      })
+      .catch(e => {
+        console.log(e);
+        this.setState({
+          nmbrOfClb: "Couldn't fetch"
         });
       });
   }
@@ -31,12 +60,63 @@ class ProjectMetrics extends Component {
     return (
       <div style={this.container}>
         <h2 style={styles.HeaderText}>Metrics</h2>
+        <div class="row" style={{ width: "100%", textAlign: "left" }}>
+          <div class="col-md-3 col-sm-12 col-lg-3">
+            <h3 style={styles.title}>{this.state.nmbrOfBugs} Bugs</h3>
+            <h6 style={styles.subtitle}>to be fixed</h6>
+          </div>
+          <div class="col-md-4 col-sm-12 col-lg-4">
+            <h3 style={styles.title}>{this.state.nmbrOfEts} Features</h3>
+            <h6 style={styles.subtitle}>to be implemented</h6>
+          </div>
+          <div class="col-md-5 col-sm-12 col-lg-5">
+            <h3 style={styles.title}>{this.state.nmbrOfClb} Contributors</h3>
+            <h6 style={styles.subtitle}>has added code</h6>
+          </div>
+        </div>
+
         <p style={styles.Description}>
-          {"This is where I'd put my Metrics component, IF I HAD ONE"}
+          {
+            "Feel free to take a look at our issues and give an helping hand. Or why not give your own suggestion for improvements and commit via Github."
+          }
         </p>
+
+        <div
+          style={{
+            display: "inline-flex",
+            textAlign: "left",
+            width: "100%"
+          }}
+        >
+          <div style={styles.buttonContainer}>
+            <button
+              onClick={() => {
+                this.openGit;
+              }}
+              className="SeeThroughBtn"
+            >
+              Get the code!
+            </button>
+          </div>
+          <div
+            style={{
+              marginLeft: 20,
+              marginTop: 20
+            }}
+          >
+            <pre style={{ color: "white", textAlign: "bottom" }}>
+              or{"  "}
+              <a style={styles.suggestionLink}>Send in a suggestion</a>
+            </pre>
+          </div>
+        </div>
       </div>
     );
   }
+
+  openGit = () => {
+    window.location = this.props.gitURL;
+  };
 }
 
 const styles = {
@@ -44,20 +124,28 @@ const styles = {
     width: "100%",
     textAlign: "left"
   },
-  full: {
-    width: "100%",
-    borderBottom: "2px solid white",
-    position: "absolute",
-    left: 0,
-    right: 0
-  },
   Description: {
     color: "white",
     fontSize: 16,
-    textAlign: "left"
+    textAlign: "left",
+    marginTop: 10
   },
   HeaderText: {
-    textAlign: "left"
+    textAlign: "left",
+    width: "100%"
+  },
+  metrics: {
+    display: "flex"
+  },
+  title: {
+    fontSize: "20px"
+  },
+  subtitle: {
+    fontSize: "12px",
+    fontStyle: "italic"
+  },
+  suggestionLink: {
+    color: "var(--bluey-green)"
   }
 };
 export default ProjectMetrics;
