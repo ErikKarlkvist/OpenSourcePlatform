f77fd1eab8ed683f945c98e32231631657e8fca9
Jostein Brevik <jbre@netlight.com>
2018-07-13 15:21:54 +0200

    Initialize CreateProjectPage

--- app/src/components/LoginRegister.js
+++ app/src/components/LoginRegister.js
@@ -2,40 +2,47 @@ import React, { Component } from "react";
 import "./AnimatedMenu.css";
 import LoginForm from "./LoginForm";
 import SignupForm from "./SignupForm";
-
-import { logout } from "../backend/auth"
+import { BrowserRouter as Router, Route, Link } from "react-router-dom";
+import { logout } from "../backend/auth";
 class LoginRegister extends Component {
-
   constructor() {
     super();
     this.state = {
       displaySignup: false,
       displayLogin: false,
-      loading: false,
-    }
+      loading: false
+    };
   }
 
   render() {
     //hooka med login
     if (!this.props.hasFetchedUser || this.state.loading) {
-      return (
-        <div />
-      )
+      return <div />;
     }
 
     if (!this.props.isLoggedIn) {
       return (
         <div>
-          {this.state.displayLogin && <LoginForm hide={this.hide} switchDisplay={this.switchDisplay} />}
-          {this.state.displaySignup && <SignupForm hide={this.hide} switchDisplay={this.switchDisplay} />}
+          {this.state.displayLogin && (
+            <LoginForm hide={this.hide} switchDisplay={this.switchDisplay} />
+          )}
+          {this.state.displaySignup && (
+            <SignupForm hide={this.hide} switchDisplay={this.switchDisplay} />
+          )}
           <div>
-            <a className={"MenuItem"} onClick={() => this.setState({ displaySignup: true })} >
+            <a
+              className={"MenuItem"}
+              onClick={() => this.setState({ displaySignup: true })}
+            >
               Sign up
-           </a>
+            </a>
             <a> | </a>
-            <a className={"MenuItem"} onClick={() => this.setState({ displayLogin: true })} >
+            <a
+              className={"MenuItem"}
+              onClick={() => this.setState({ displayLogin: true })}
+            >
               Log in
-           </a>
+            </a>
           </div>
           <div style={styles.line} />
         </div>
@@ -43,27 +50,29 @@ class LoginRegister extends Component {
     } else {
       return (
         <div style={styles.container}>
-          <h5>Welcome {this.props.user.firstname} {this.props.user.lastname}</h5>
+          <h5>
+            Welcome {this.props.user.firstname} {this.props.user.lastname}
+          </h5>
           <div style={styles.line} />
           <div style={styles.optionsContainer}>
             <div style={styles.option}>
-              <a className={"MenuItem"} onClick={() => { }} >
-                Create Project
-              </a>
+              <Link to={`\createProject`}>
+                <a className={"MenuItem"}>Create Project</a>
+              </Link>
             </div>
             <div style={styles.option}>
-              <a className={"MenuItem"} onClick={() => { }} >
+              <a className={"MenuItem"} onClick={() => {}}>
                 Your Projects
               </a>
             </div>
             <div style={styles.option}>
-              <a className={"MenuItem"} onClick={this.logout} >
+              <a className={"MenuItem"} onClick={this.logout}>
                 Log out
               </a>
             </div>
           </div>
         </div>
-      )
+      );
     }
   }
 
@@ -71,34 +80,34 @@ class LoginRegister extends Component {
     if (this.state.displaySignup) {
       this.setState({
         displaySignup: false,
-        displayLogin: true,
-      })
+        displayLogin: true
+      });
     } else {
       this.setState({
         displaySignup: true,
-        displayLogin: false,
-      })
+        displayLogin: false
+      });
     }
-  }
+  };
 
   logout = () => {
-    this.setState({ loading: true })
+    this.setState({ loading: true });
     logout().then(() => {
-      this.setState({ loading: false })
-    })
-  }
+      this.setState({ loading: false });
+    });
+  };
 
   hide = () => {
     this.setState({
       displaySignup: false,
-      displayLogin: false,
-    })
-  }
+      displayLogin: false
+    });
+  };
 }
 
 const styles = {
   line: {
-    borderBottom: '1px solid white',
+    borderBottom: "1px solid white",
     height: "20px",
     width: "150px",
     marginLeft: "50px",
@@ -107,11 +116,11 @@ const styles = {
     top: 40
   },
   optionsContainer: {
-    marginTop: 30,
+    marginTop: 30
   },
   option: {
     marginTop: 10,
     textAlign: "right"
   }
-}
+};
 export default LoginRegister;
