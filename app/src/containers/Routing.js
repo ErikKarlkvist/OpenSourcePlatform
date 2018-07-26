da7d05dc078e55e173dc1b59793da9fa829d5c32
Erik Karlkvist <erka@netlight.com>
2018-07-26 12:50:15 +0200

    option fix

--- app/src/containers/Routing.js
+++ app/src/containers/Routing.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { HashRouter as Router, Route, Link } from "react-router-dom";
+import { BrowserRouter as Router, Route, Link } from "react-router-dom";
 import { withRouter } from "react-router";
 import Home from "./pages/HomePage";
 import Project from "./pages/ProjectPage";
